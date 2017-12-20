c01794abe165e8fdffa6c84f03110a9377fa807e
Johnny <157781745@qq.com>
2017-12-21 03:59:10 +0800

    only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,26 +7,30 @@ use App\Models\User;
 use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandler;
 
-class UsersController extends Controller
-{
+class UsersController extends Controller {
+    public function __construct() {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     //
-    public function show(User $user)
-    {
+    public function show(User $user) {
         return view('users.show', compact('user'));
     }
-    public function edit(User $user)
-    {
+
+    public function edit(User $user) {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
-    public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
-    {
+
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user) {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id,362);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
             if ($result) {
                 $data['avatar'] = $result->path;
-                $uploader->crop($result->file,362,362);
+                $uploader->crop($result->file, 362, 362);
             }
         }
 
